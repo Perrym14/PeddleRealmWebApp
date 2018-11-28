34d3dad0824a0af17006233937647bdadbe4ce56
Malik Perry <Malikap14@gmail.com>
2018-11-28 14:23:19 -0800

    Added dropdown link in 'login_partial' view.

---
+++ PeddleRealmWebApp/Controllers/AdminController.cs
@@ -0,0 +1,27 @@
+﻿using PeddleRealmWebApp.Models;
+using PeddleRealmWebApp.ViewModels;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace PeddleRealmWebApp.Controllers
+{
+    public class AdminController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            var viewModel = new ItemViewModel
+            {
+                ItemTypes = _context.ItemTypes.ToList()
+            };
+            return View(viewModel);
+        }
+    }
+}
---
+++ PeddleRealmWebApp/ViewModels/ItemViewModel.cs
@@ -0,0 +1,29 @@
+﻿using PeddleRealmWebApp.Models;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Web;
+
+namespace PeddleRealmWebApp.ViewModels
+{
+    public class ItemViewModel
+    {
+
+        public string Name { get; set; }
+
+
+        public decimal Price { get; set; }
+
+
+        public byte ItemType { get; set; }
+
+        public IEnumerable<ItemType> ItemTypes { get; set; }
+
+
+        public string Description { get; set; }
+
+        [Required]
+        [DataType(DataType.Upload)]
+        public HttpPostedFileBase ItemPhoto { get; set; }
+
+    }
+}
