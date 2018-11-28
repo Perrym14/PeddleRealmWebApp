ecfb4268322488cdb9101fe83bc0f12bc596068d
Malik Perry <Malikap14@gmail.com>
2018-11-28 12:20:17 -0800

    Added 'Item' and 'ItemType' models. Also moved 'ApplicationDbContext' and 'ApplicationUser'.

---
+++ PeddleRealmWebApp/Models/Item.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PeddleRealmWebApp.Models
+{
+    public class Item
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        [Required]
+        public ItemType ItemType { get; set; }
+
+        [Required]
+        public decimal Price { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Description { get; set; }
+    }
+}
---
+++ PeddleRealmWebApp/Models/ItemType.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PeddleRealmWebApp.Models
+{
+    public class ItemType
+    {
+        public byte Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+    }
+}
